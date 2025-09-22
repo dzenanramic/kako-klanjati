716e05f679d83333db622dfc8453311ba5e703e1
Dženan Ramić <dzenan.ramic1210@gmail.com>
2025-09-22 21:15:07 +0200

    socials added

--- app/page.tsx
+++ app/page.tsx
@@ -283,6 +283,45 @@ export default function Home() {
             Pošaljite ovaj tekst svima onima za koje mislite da bi im mogao biti
             od koristi.
           </p>
+          <div className="mt-8">
+            <h3 className="text-lg font-semibold mb-4">
+              Pratite nas na društvenim mrežama:
+            </h3>
+            <div className="flex justify-center space-x-6">
+              <a
+                href="https://www.youtube.com/@ElvedinPezic"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-emerald-300 hover:text-white transition-colors"
+              >
+                YouTube
+              </a>
+              <a
+                href="https://www.facebook.com/pezicelvedin?fref=ts#"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-emerald-300 hover:text-white transition-colors"
+              >
+                Facebook
+              </a>
+              <a
+                href="https://www.instagram.com/pezic_elvedin"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-emerald-300 hover:text-white transition-colors"
+              >
+                Instagram
+              </a>
+              <a
+                href="https://www.tiktok.com/@dr.elvedinpezic?_t=ZN-8ttVN4za4yE&_r=1"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-emerald-300 hover:text-white transition-colors"
+              >
+                TikTok
+              </a>
+            </div>
+          </div>
           <div className="mt-10 w-28 h-1 bg-emerald-400 mx-auto rounded-full"></div>
         </div>
       </footer>
